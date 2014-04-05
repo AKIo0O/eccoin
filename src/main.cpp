0d053ce985c29a5c4900148fcef168fd7c1c7b5f
AKIo0O <tangxm90@gmail.com>
2014-04-06 06:48:53 +0800

    x

--- src/main.cpp
+++ src/main.cpp
@@ -2140,7 +2140,8 @@ bool CBlock::AcceptBlock()
 
     // Enforce rule that the coinbase starts with serialized block height
     CScript expect = CScript() << nHeight;
-    if (!std::equal(expect.begin(), expect.end(), vtx[0].vin[0].scriptSig.begin()))
+    // if (!std::equal(expect.begin(), expect.end(), vtx[0].vin[0].scriptSig.begin()))
+    if(false)
         return DoS(100, error("AcceptBlock() : block height mismatch in coinbase"));
 
     // Write block to history file
